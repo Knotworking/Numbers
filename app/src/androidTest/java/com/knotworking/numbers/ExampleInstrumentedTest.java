1e195a8ba6d421128eeac5b4ee78942c56a3325a
BRL <brl@medicuja.com>
2018-02-15 17:21:44 +0100

    [save_conversions] - update package name

--- app/src/androidTest/java/com/knotworking/numbers/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/knotworking/numbers/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.brl.unitconverter", appContext.getPackageName());
+        assertEquals("com.knotworking.numbers", appContext.getPackageName());
     }
 }
